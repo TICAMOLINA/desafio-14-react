103bde83ec6803058aa85617190965ea66772d5d
Mauricio Molina <tica.molina57@gmail.com>
2025-02-15 23:50:46 -0300

    Instalo la Tailwind y le aplico estilos a la Usercard

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import TarjetaUsuario from "./components/TarjetaUsuario"
 
 const App = () => {
   return (
-    <div>
-      <h1>Desafío nro. 14</h1>
+    <div className="container mx-auto">
+      <h1 className="text-[40px] font-bold text-center mb-6">Desafío nro. 14</h1>
 
-      <h2>Ejercicio A</h2>
+      <h2 className="text-[30px] mb-4">Ejercicio A</h2>
       <TarjetaUsuario nombre="Mauricio Molina" edad="35 años" profesion="Full Stack Engineer" />
 
     </div>
--- src/components/TarjetaUsuario.jsx
+++ src/components/TarjetaUsuario.jsx
@@ -2,10 +2,12 @@
 const TarjetaUsuario = (props) => {
 
   return (
-    <div>
-        <h3>{props.nombre}</h3>
-        <p>{props.edad}</p>
-        <p>{props.profesion}</p>
+    <div className="w-[500px] h-[200px] bg-blue-400 rounded-2xl ring-2 text-center">
+        <h3 className="uppercase p-7 text-[20px] text-white mb-3 mt-5">
+            {props.nombre}
+        </h3>
+        <p className="text-gray-200 text-[20px]">{props.edad}</p>
+        <p className="text-gray-200 text-[20px]">{props.profesion}</p>
 
     </div>
   )
